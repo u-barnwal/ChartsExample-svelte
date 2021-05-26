c0cb7a88f2c7dd646876a209182805c3832afc4e
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-27 00:58:23 +0530

    add empty Chart and ChartContainer

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,15 @@
 <script>
+  import Charts from "./Charts.svelte";
+
   export let name;
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <Charts />
 </main>
 
 <style global lang="postcss">
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
-
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
 </style>
---
+++ src/components/ChartContainer.svelte
@@ -0,0 +1,5 @@
+<div class="w-1/3 inline-block text-center p-3">
+  <div class="border border-brand-gray p-5 rounded-md h-full">
+    <slot />
+  </div>
+</div>
