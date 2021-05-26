778cf1f14d311545167015e64e566e2afa5005cb
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-27 01:46:27 +0530

    update title and fix style warning

--- src/App.svelte
+++ src/App.svelte
@@ -6,13 +6,6 @@
   <Charts />
 </main>
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://unpkg.com/@carbon/charts/styles.min.css"
-  />
-</svelte:head>
-
 <style global lang="postcss">
   @tailwind base;
   @tailwind components;
