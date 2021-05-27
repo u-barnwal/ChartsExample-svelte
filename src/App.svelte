19687842873624d7a6a05ef10cb4684a9ee97030
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-28 02:43:29 +0530

    add Footer and clear default styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import Charts from "./Charts.svelte";
+  import Footer from "./Footer.svelte";
 </script>
 
 <main>
   <Charts />
+  <Footer />
 </main>
 
 <style global lang="postcss">
---
+++ src/Footer.svelte
@@ -0,0 +1,27 @@
+<div class="mb-10 flex justify-center w-full">
+  <a
+    target="_blank"
+    href="https://github.com/u-barnwal/TodoList-svelte/"
+    title="View on GitHub"
+  >
+    <img
+      src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+      alt="github"
+    />
+  </a>
+
+  <a target="_blank" href="https://utsavbarnwal.me/" title="Developer"
+    ><img src="https://utsavbarnwal.me/images/user.jpeg" alt="developer" /></a
+  >
+</div>
+
+<style>
+  img {
+    width: 2em;
+    height: 2em;
+    object-fit: cover;
+    object-position: top;
+    border-radius: 50%;
+    margin: 0 0.5em;
+  }
+</style>
