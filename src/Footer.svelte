24dde9ff42d1467a66095cc48b8ee160b01b2a45
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-28 03:57:52 +0530

    update footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,18 +1,26 @@
-<div class="mb-10 flex justify-center w-full">
-  <a
-    target="_blank"
-    href="https://github.com/u-barnwal/TodoList-svelte/"
-    title="View on GitHub"
-  >
-    <img
-      src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-      alt="github"
-    />
-  </a>
+<div class="mb-10">
+  <div class="font-bold text-center mb-1">Svelte Charts Example</div>
+  <div class="text-xs text-center mb-4">
+    A single-page web app to try different charts libraries for Svelte and
+    compare them.
+  </div>
 
-  <a target="_blank" href="https://utsavbarnwal.me/" title="Developer"
-    ><img src="https://utsavbarnwal.me/images/user.jpeg" alt="developer" /></a
-  >
+  <div class="flex justify-center w-full">
+    <a
+      target="_blank"
+      href="https://github.com/u-barnwal/ChartsExample-svelte"
+      title="View on GitHub"
+    >
+      <img
+        src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+        alt="github"
+      />
+    </a>
+
+    <a target="_blank" href="https://utsavbarnwal.me/" title="Developer"
+      ><img src="https://utsavbarnwal.me/images/user.jpeg" alt="developer" /></a
+    >
+  </div>
 </div>
 
 <style>
