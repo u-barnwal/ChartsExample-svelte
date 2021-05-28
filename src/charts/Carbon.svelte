720901fac9c45ece0e58f06b588aae656267b56f
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-28 20:25:59 +0530

    groupped Carbon chart

--- src/charts/Carbon.svelte
+++ src/charts/Carbon.svelte
@@ -1,4 +1,14 @@
 <script>
+  import {
+    BarChartSimple,
+    DonutChart,
+    GaugeChart,
+    LineChart,
+    PieChart,
+    ComboChart,
+  } from "@carbon/charts-svelte";
+
+  import ChartContainer from "../components/ChartContainer.svelte";
   import SpecsBox from "../components/SpecsBox.svelte";
   import SpecItem from "../components/SpecItem.svelte";
 </script>
@@ -46,4 +56,179 @@
     <SpecItem con>Not Exportable</SpecItem>
     <SpecItem con>Not Animateable</SpecItem>
   </SpecsBox>
+
+  <ChartContainer>
+    <BarChartSimple
+      data={[
+        { group: "Qty", date: "2018-12-31T18:30:00.000Z", value: 10000 },
+        { group: "More", date: "2019-01-01T18:30:00.000Z", value: 65000 },
+        { group: "Sold", date: "2019-01-02T18:30:00.000Z", value: 30000 },
+        {
+          group: "Restocking",
+          date: "2019-01-05T18:30:00.000Z",
+          value: 49213,
+        },
+        { group: "Misc", date: "2019-01-06T18:30:00.000Z", value: 51213 },
+      ]}
+      options={{
+        title: "Vertical simple bar (time series)",
+        axes: {
+          left: { mapsTo: "value" },
+          bottom: { mapsTo: "date", scaleType: "time" },
+        },
+        height: "400px",
+      }}
+    />
+  </ChartContainer>
+
+  <ChartContainer>
+    <DonutChart
+      data={[
+        { group: "2V2N 9KYPM version 1", value: 20000 },
+        { group: "L22I P66EP L22I P66EP L22I P66EP", value: 65000 },
+        { group: "JQAI 2M4L1", value: 75000 },
+        { group: "J9DZ F37AP", value: 1200 },
+        { group: "YEL48 Q6XK YEL48", value: 10000 },
+        { group: "Misc", value: 25000 },
+      ]}
+      options={{
+        title: "Donut (centered)",
+        resizable: true,
+        legend: { alignment: "center" },
+        donut: { center: { label: "Browsers" }, alignment: "center" },
+        height: "400px",
+      }}
+    />
+  </ChartContainer>
+
+  <ChartContainer>
+    <PieChart
+      data={[
+        { group: "2V2N 9KYPM version 1", value: 20000 },
+        { group: "L22I P66EP L22I P66EP L22I P66EP", value: 65000 },
+        { group: "JQAI 2M4L1", value: 75000 },
+        { group: "J9DZ F37AP", value: 1200 },
+        { group: "YEL48 Q6XK YEL48", value: 10000 },
+        { group: "Misc", value: 25000 },
+      ]}
+      options={{
+        title: "Pie (centered)",
+        resizable: true,
+        legend: { alignment: "center" },
+        pie: { alignment: "center" },
+        height: "400px",
+      }}
+    />
+  </ChartContainer>
+
+  <ChartContainer>
+    <LineChart
+      data={[
+        { group: "Dataset 1", key: "Qty", value: 34200 },
+        { group: "Dataset 1", key: "More", value: 23500 },
+        { group: "Dataset 1", key: "Sold", value: 53100 },
+        { group: "Dataset 1", key: "Restocking", value: 42300 },
+        { group: "Dataset 1", key: "Misc", value: 12300 },
+        { group: "Dataset 2", key: "Qty", value: 34200 },
+        { group: "Dataset 2", key: "More", value: 53200 },
+        { group: "Dataset 2", key: "Sold", value: 42300 },
+        { group: "Dataset 2", key: "Restocking", value: 21400 },
+        { group: "Dataset 2", key: "Misc", value: 0 },
+        { group: "Dataset 3", key: "Qty", value: 41200 },
+        { group: "Dataset 3", key: "More", value: 18400 },
+        { group: "Dataset 3", key: "Sold", value: 34210 },
+        { group: "Dataset 3", key: "Restocking", value: 1400 },
+        { group: "Dataset 3", key: "Misc", value: 42100 },
+        { group: "Dataset 4", key: "Qty", value: 22000 },
+        { group: "Dataset 4", key: "More", value: 1200 },
+        { group: "Dataset 4", key: "Sold", value: 9000 },
+        {
+          group: "Dataset 4",
+          key: "Restocking",
+          value: 24000,
+          audienceSize: 10,
+        },
+        { group: "Dataset 4", key: "Misc", value: 3000, audienceSize: 10 },
+      ]}
+      options={{
+        title: "Line (discrete)",
+        axes: {
+          bottom: {
+            title: "2019 Annual Sales Figures",
+            mapsTo: "key",
+            scaleType: "labels",
+          },
+          left: {
+            mapsTo: "value",
+            title: "Conversion rate",
+            scaleType: "linear",
+          },
+        },
+        height: "400px",
+      }}
+    />
+  </ChartContainer>
+
+  <ChartContainer>
+    <ComboChart
+      data={[
+        { group: "School A", date: "Monday", value: 10000 },
+        { group: "School A", date: "Tuesday", value: 65000 },
+        { group: "School A", date: "Wednesday", value: 30000 },
+        { group: "School A", date: "Thursday", value: 49213 },
+        { group: "School A", date: "Friday", value: 49213 },
+        { group: "Temperature", date: "Monday", temp: 70 },
+        { group: "Temperature", date: "Tuesday", temp: 75 },
+        { group: "Temperature", date: "Wednesday", temp: 31 },
+        { group: "Temperature", date: "Thursday", temp: 31 },
+        { group: "Temperature", date: "Friday", temp: 43 },
+      ]}
+      options={{
+        title: "Combo (Line + Simple bar) - custom configs",
+        axes: {
+          left: {
+            mapsTo: "value",
+            scaleType: "linear",
+            title: "USA Summer School Attendance",
+          },
+          right: {
+            mapsTo: "temp",
+            scaleType: "linear",
+            title: "Temperature (°F)",
+            correspondingDatasets: ["Temperature"],
+          },
+          bottom: {
+            title: "Day of the Week",
+            mapsTo: "date",
+            scaleType: "labels",
+          },
+        },
+        comboChartTypes: [
+          { type: "simple-bar", correspondingDatasets: ["School A"] },
+          {
+            type: "line",
+            options: { points: { radius: 5 } },
+            correspondingDatasets: ["Temperature"],
+          },
+        ],
+        height: "400px",
+      }}
+    />
+  </ChartContainer>
+
+  <ChartContainer>
+    <GaugeChart
+      data={[
+        { group: "value", value: 42 },
+        { group: "delta", value: -13.37 },
+      ]}
+      options={{
+        title: "Gauge semicircular -- danger status",
+        resizable: true,
+        height: "250px",
+        width: "100%",
+        gauge: { type: "semi", status: "danger" },
+      }}
+    />
+  </ChartContainer>
 </div>
--- src/components/ChartContainer.svelte
+++ src/components/ChartContainer.svelte
@@ -1,4 +1,4 @@
-<div class="w-full lg:w-1/3 inline-block text-center p-3">
+<div class="w-full inline-block text-center py-3">
   <div class="border border-brand-gray p-5 rounded-md h-full shadow-sm">
     <slot />
   </div>
